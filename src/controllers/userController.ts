1c8462acf9fdef303816ce11b4d232f765e3da38
Chiranjibi Khanal <chiranjibik95@gmail.com>
2025-10-30 19:06:22 +0545

    hashing

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,32 +1,33 @@
-import { Request,Response } from "express";
-import User from "../database/models/userModel";
-import { promises } from "dns";
+    import { Request,Response } from "express";
+    import User from "../database/models/userModel";
+    import { promises } from "dns";
+    import bcrypt from 'bcrypt'
 
 
 
-class AuthController{
-    public static async registerUser(req:Request,res:Response):Promise<void>{
+    class AuthController{
+        public static async registerUser(req:Request,res:Response):Promise<void>{
 
-        const{username,email,password}=req.body
-        if(!username || !email || !password){
-            res.status(400).json({
-                message:"Please provide user name password and email"
+            const{username,email,password}=req.body
+            if(!username || !email || !password){
+                res.status(400).json({
+                    message:"Please provide user name password and email"
+                })
+                return
+            }
+
+            await User.create({
+                username,
+                email,
+                password : bcrypt.hashSync(password,8)
             })
-            return
+
+            res.status(200).json({
+                message:"User register Sucessful"
+            })
         }
-
-        await User.create({
-            username,
-            email,
-            password
-        })
-
-        res.status(200).json({
-            message:"User register Sucessful"
-        })
+        
     }
-    
-}
 
 
-export default AuthController
+    export default AuthController
