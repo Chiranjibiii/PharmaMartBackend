d3946ab5bd131905b1fc733a486f7cf09b2ec7a2
Chiranjibi Khanal <chiranjibik95@gmail.com>
2025-11-10 13:10:19 +0545

    category crud

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,6 +1,6 @@
+import { where } from "sequelize";
 import Category from "../database/models/category";
-
-
+import { Request,Response } from "express";
 
 class CategoryController {
     categoryData = [
@@ -32,6 +32,65 @@ class CategoryController {
             
         }
        
+    }
+    async addCategory(req:Request,res:Response):Promise<void>{
+        const {categoryName}=req.body
+        if(!categoryName ){
+            res.status(400).json({
+                message:"please provide categoryName"
+            })
+            return
+
+        }
+        await Category.create({
+            categoryName
+        })
+        res.status(200).json({
+            message:"Category Added sucessfully"
+        })
+    }
+    async getCategory(req:Request,res:Response):Promise<void>{
+        const data=Category.findAll()
+        res.status(200).json({
+            message:"Category fetched sucessfully",
+            data
+        })
+    }
+    async deleteCategory(req:Request,res:Response):Promise<void>{
+        const {id}=req.params
+        const data= await Category.findAll({
+            where :{
+                id
+            }
+        })
+        if(data.length === 0){
+            res.status(404).json({
+                message:"No category with that Id"
+            })
+            
+        }
+        else{
+            await Category.destroy({
+                where :{
+                    id : id
+                }
+            })
+            res.status(200).json({
+                message:"Category Deleted Sucessfully"
+            })
+        }
+    }
+    async updateCategory(req:Request,res:Response):Promise<void>{
+        const {id}=req.params
+        const {categoryName}=req.body
+        await Category.update({categoryName},{
+            where : {
+                id
+            }
+        })
+        res.status(200).json({
+            message:"Category Updated"
+        })
     }
 }
 
