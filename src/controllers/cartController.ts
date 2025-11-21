9d00d44ee759ebc2355f2e261c3529bb468b64c1
Chiranjibi Khanal <chiranjibik95@gmail.com>
2025-11-21 18:15:02 +0545

    Order table

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -2,79 +2,82 @@ import {Request,Response} from 'express'
 import { AuthRequest } from '../middleware/authMiddleware'
 import Cart from '../database/models/cart'
 import Product from '../database/models/product'
-import { Model } from 'sequelize'
 import Category from '../database/models/category'
 
 
-
 class CartController{
+
     async addToCart(req:AuthRequest,res:Response):Promise<void>{
         const userId=req.user?.id
         const {quantity,productId}=req.body
+        
         if(!quantity || !productId){
             res.status(400).json({
                 message:"Please Provide quantity and ProductId"
             })
+            return
         }
-        //check if the product already exists in the cart table
-        let cartItem=await Cart.findOne({
-            where:{
-                productId,
-                userId
-            }
-        })
-        if(cartItem){
-            cartItem.quantity+=quantity
-            await cartItem.save()
 
-        }else{  
-            cartItem=await Cart.create({
+        let cartItem=await Cart.findOne({
+            where:{ productId, userId }
+        })
+
+        if(cartItem){
+            cartItem.quantity += quantity
+            await cartItem.save()
+        }else{
+            cartItem = await Cart.create({
                 quantity,
                 userId,
                 productId
             })
         }
-            res.status(200).json({
-                message:"Product added to Cart",
-                data:cartItem
-            })
-        
+
+        res.status(200).json({
+            message:"Product added to Cart",
+            data:cartItem
+        })
     }
+
+
     async getyMyCarts(req:AuthRequest,res:Response):Promise<void>{
         const userId=req.user?.id
+        
         const cartItems=await Cart.findAll({
-            where:{
-                userId
-            },
+            where:{ userId },
             include:[
                 {
                     model:Product,
+                    attributes:['productName','productDescription','productImageUrl'],
                     include:[
                         {
-                            model : Category,
-                            attributes:['id','categoryName '] 
+                            model: Category,
+                            attributes:['id','categoryName']    
                         }
                     ]
                 }
-            ]
+            ],
+            attributes : ['productId','quantity']
         })
+
         if(cartItems.length===0){
             res.status(400).json({
                 message:"No Item in the cart"
             })
-        }else{
-            res.status(200).json({
-                message:"Cart Item fetched Sucessfully",
-                data:cartItems
-            })
+            return
         }
+
+        res.status(200).json({
+            message:"Cart Item fetched Sucessfully",
+            data:cartItems
+        })
     }
+
 
     async deleteMyCartItem(req:AuthRequest,res:Response):Promise<void>{
         const userId=req.user?.id
-        const{productId} = req.params
+        const {productId} = req.params
 
-        //check whether above porductId product exits or not 
         const product = await Product.findByPk(productId)
         if(!product){
             res.status(404).json({
@@ -82,48 +85,47 @@ class CartController{
             })
             return
         }
-        //delete that product from userCart
+
         await Cart.destroy({
-            where:{
-                userId,
-                productId
-            }
+            where:{ userId, productId }
         })
+
         res.status(200).json({
             message:"Product Delete sucessfully from the cart "
         })
     }
 
+
     async updateCartItem(req:AuthRequest,res:Response):Promise<void>{
         const {productId} = req.params
         const userId=req.user?.id
-        const{quantity}=req.body
+        const {quantity}=req.body
+
         if(!quantity){
             res.status(400).json({
                 message:"Please Provide Quantity"
             })
             return
         }
+
         const cartData = await Cart.findOne({
-            where:{
-                userId,
-                productId
-            }
+            where:{ userId, productId }
         })
 
-        if(cartData){ cartData.quantity=quantity
-        await cartData?.save()
-        res.status(200).json({
-            message:"Product of the cart Updated",
-            data:cartData 
-        })
-    }else{
-        res.status(404).json({
-            message:"No productId of that userId"
-        })
+        if(cartData){
+            cartData.quantity = quantity
+            await cartData.save()
+
+            res.status(200).json({
+                message:"Product of the cart Updated",
+                data:cartData 
+            })
+        }else{
+            res.status(404).json({
+                message:"No productId of that userId"
+            })
+        }
     }
 }
-}
-
 
 export default new CartController()
